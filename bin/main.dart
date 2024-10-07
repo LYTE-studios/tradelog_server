72a546c5bd1726c9d147893879e92853047ef99b
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-10-07 16:09:47 +0200

    feat: sentry + mailjet templates

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,16 @@
+import 'package:sentry/sentry.dart';
 import 'package:tradelog_server/server.dart';
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
-void main(List<String> args) {
+void main(List<String> args) async {
+  await Sentry.init((options) {
+    options.dsn =
+        'https://bff1f7d2e66af31a22585aa34a756c1d@o4506789659475968.ingest.us.sentry.io/4508081693982720';
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+    // We recommend adjusting this value in production.
+    options.tracesSampleRate = 1.0;
+  });
+
   run(args);
 }
